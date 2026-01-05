92cedca93f83cec38e6f885e028550723348783b
Aslan <tynyshtykaslan06@gmail.com>
2026-01-06 00:37:16 +0500

    inventory-service

---
+++ services/inventory-service/src/main/java/com/project/ecommerce/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.project.ecommerce.repository;
+
+import com.project.ecommerce.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
