e87b41a95ba87272cfa5ae02b10931e1d0068171
Aslan <tynyshtykaslan06@gmail.com>
2026-01-06 23:32:26 +0500

    inventory client (feign client) api gateway init

--- services/order-service/src/main/java/com/project/ecommerce/service/OrderService.java
+++ services/order-service/src/main/java/com/project/ecommerce/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.project.ecommerce.service;
 
+import com.project.ecommerce.client.InventoryClient;
 import com.project.ecommerce.dto.OrderRequest;
 import com.project.ecommerce.model.Order;
 import com.project.ecommerce.repository.OrderRepository;
@@ -15,13 +16,24 @@ import java.util.UUID;
 @Slf4j
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
+
 
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+
+        var isInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if(isInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        } else {
+            throw new IllegalStateException(
+                    "Product " + orderRequest.skuCode() + " is not in stock"
+            );
+        }
     }
 }
