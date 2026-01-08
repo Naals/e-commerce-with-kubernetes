ad66089c6de8dd232027bfebbc91a845aba51a5a
Aslan <tynyshtykaslan06@gmail.com>
2026-01-08 10:53:47 +0500

    kafka init

--- services/order-service/src/main/java/com/project/ecommerce/service/OrderService.java
+++ services/order-service/src/main/java/com/project/ecommerce/service/OrderService.java
@@ -2,10 +2,12 @@ package com.project.ecommerce.service;
 
 import com.project.ecommerce.client.InventoryClient;
 import com.project.ecommerce.dto.OrderRequest;
+import com.project.ecommerce.event.OrderPlacedEvent;
 import com.project.ecommerce.model.Order;
 import com.project.ecommerce.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -17,6 +19,7 @@ import java.util.UUID;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
 
     public void placeOrder(OrderRequest orderRequest) {
@@ -30,6 +33,15 @@ public class OrderService {
             order.setSkuCode(orderRequest.skuCode());
             order.setQuantity(orderRequest.quantity());
             orderRepository.save(order);
+
+            OrderPlacedEvent event = new OrderPlacedEvent();
+            event.setOrderNumber(orderRequest.skuCode());
+            event.setFirstName(orderRequest.userDetails().firstName());
+            event.setLastName(orderRequest.userDetails().lastName());
+            event.setEmail(orderRequest.userDetails().email());
+            log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", event);
+            kafkaTemplate.send("order-placed", event);
+            log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", event);
         } else {
             throw new IllegalStateException(
                     "Product " + orderRequest.skuCode() + " is not in stock"
